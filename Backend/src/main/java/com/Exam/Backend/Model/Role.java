c6615127d662abbc07be8128045f00e838b71e19
Amit <amithegde777@gmail.com>
2024-01-13 13:02:07 +0530

    add entities

---
+++ Backend/src/main/java/com/Exam/Backend/Model/Role.java
@@ -0,0 +1,50 @@
+package com.Exam.Backend.Model;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    private Long roleId;
+    private String roleName;
+
+    @OneToMany(cascade = CascadeType.ALL,fetch = FetchType.LAZY,mappedBy = "role")
+    private Set<userRole> userRoles=new HashSet<>();
+
+    public Set<userRole> getUserRoles() {
+        return userRoles;
+    }
+
+    public void setUserRoles(Set<userRole> userRoles) {
+        this.userRoles = userRoles;
+    }
+
+    public Role() {
+    }
+
+    public Role(Long roleId, String roleName) {
+        this.roleId = roleId;
+        this.roleName = roleName;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
---
+++ Backend/src/main/java/com/Exam/Backend/Model/userRole.java
@@ -0,0 +1,45 @@
+package com.Exam.Backend.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class userRole {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userRoleId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Role role;
+
+    public userRole() {
+
+    }
+
+    public Long getUserRoleId() {
+        return userRoleId;
+    }
+
+    public void setUserRoleId(Long userRoleId) {
+        this.userRoleId = userRoleId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
