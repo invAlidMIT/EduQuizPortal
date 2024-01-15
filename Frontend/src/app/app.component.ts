b40a19aba9724224af4f27ee7a8c6f5fb3f181e5
Amit <amithegde777@gmail.com>
2024-01-15 23:52:45 +0530

    add two-way data bind

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -4,13 +4,13 @@ import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,MatButtonModule,MatInputModule,MatFormFieldModule],
+  imports: [CommonModule, RouterOutlet,MatButtonModule,MatInputModule,MatFormFieldModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/src/app/pages/signup/signup.component.ts
+++ Frontend/src/app/pages/signup/signup.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [MatInputModule,MatFormFieldModule,MatIconModule,MatButtonModule],
+  imports: [MatInputModule,MatFormFieldModule,MatIconModule,MatButtonModule,FormsModule,CommonModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+  constructor(){}
+
+  public user={
+    username:'',
+    firstname:'',
+    lastname:'',
+    password:'',
+    email:'',
+    phno:''
+  }
+
+  ngOnInit(): void {
+     
+  }
+  formSubmit(){
+    alert("submitted");
+  }
+
 
 }
