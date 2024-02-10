34a941f6abb06517cde80896b2a96a1f17d9cacd
Amit <amithegde777@gmail.com>
2024-02-11 00:35:21 +0530

    add profile page

--- Frontend/src/app/profile/profile.component.ts
+++ Frontend/src/app/profile/profile.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import { LoginService } from '../services/login.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatCardModule,MatListModule,MatIconModule,MatButtonModule,MatTableModule],
+  imports: [MatCardModule,MatListModule,MatIconModule,MatButtonModule,MatTableModule,CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+   user:any;
+  constructor(private login:LoginService){}
 
+  ngOnInit(): void {
+    this.user=this.login.getUser();
+  }
 }
