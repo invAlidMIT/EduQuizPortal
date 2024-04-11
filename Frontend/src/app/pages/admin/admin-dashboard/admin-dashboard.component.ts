49395e499cab532baf6f156e8a2a81db747d23c8
Amit <amithegde777@gmail.com>
2024-04-11 18:17:31 +0530

    enhanced sidebar

--- Frontend/src/app/pages/admin/admin-dashboard/admin-dashboard.component.ts
+++ Frontend/src/app/pages/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { NavbarComponent } from "../../../components/navbar/navbar.component";
 import {MatListModule} from '@angular/material/list';
 import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterLink } from '@angular/router';
+import { RouterLink,RouterLinkActive } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatMenuModule} from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import {MatBadgeModule} from '@angular/material/badge';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-admin-dashboard',
@@ -17,13 +18,26 @@ import { RouterOutlet } from '@angular/router';
     templateUrl: './admin-dashboard.component.html',
     styleUrl: './admin-dashboard.component.css',
     imports: [NavbarComponent, MatListModule, MatCardModule,RouterOutlet
-    ,MatSidenavModule,MatIconModule,MatButtonModule,RouterLink,MatMenuModule,MatToolbarModule,MatBadgeModule]
+    ,MatSidenavModule,MatIconModule,MatButtonModule,RouterLink,MatMenuModule,MatToolbarModule,MatBadgeModule,
+CommonModule,RouterLinkActive]
 })
 export class AdminDashboardComponent {
-    isSidebarOpen = true;
+    isSidebarOpen = signal(false);
+    dashboardActive = true;
+
+    sidebarWidth=computed(()=>this.isSidebarOpen()?'65px':'');
 
     toggleSidebar() {
-      this.isSidebarOpen = !this.isSidebarOpen;
+      this.isSidebarOpen.set(!this.isSidebarOpen());
     }
+
+    toggleDashboardDeActive() {
+        this.dashboardActive = false;
+    }
+
+    toggleDashboardActive(){
+        this.dashboardActive=true;
+    }
+    
 
 }
