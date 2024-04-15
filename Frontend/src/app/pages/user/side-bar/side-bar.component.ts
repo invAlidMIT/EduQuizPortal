6f843901352848da110ba273fe578020fc532e1a
Amit <amithegde777@gmail.com>
2024-04-15 23:46:01 +0530

    minor changes in UI

--- Frontend/src/app/pages/user/side-bar/side-bar.component.ts
+++ Frontend/src/app/pages/user/side-bar/side-bar.component.ts
@@ -1,20 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { Component, OnInit, computed, signal } from '@angular/core';
+import { NavbarComponent } from "../../../components/navbar/navbar.component";
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink,RouterLinkActive } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatBadgeModule} from '@angular/material/badge';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { CategoryService } from '../../../services/category.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+
+
 
 @Component({
-  selector: 'app-side-bar',
-  standalone: true,
-  imports: [MatCardModule,MatListModule,MatIconModule,MatButtonModule,
-  CommonModule,RouterLink],
-  templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.css'
+    selector: 'app-side-bar',
+    standalone: true,
+    templateUrl: './side-bar.component.html',
+    styleUrl: './side-bar.component.css',
+    imports: [MatCardModule, MatListModule, MatIconModule, MatButtonModule,
+        CommonModule, RouterLink, MatSidenavModule, MatMenuModule, MatToolbarModule,
+        MatBadgeModule, NavbarComponent,RouterOutlet,RouterLinkActive]
 })
 export class SideBarComponent implements OnInit {
 
@@ -37,6 +46,23 @@ export class SideBarComponent implements OnInit {
       })
     }
     )
+  }
+
+  isSidebarOpen = signal(false);
+  dashboardActive = true;
+
+  sidebarWidth=computed(()=>this.isSidebarOpen()?'65px':'');
+
+  toggleSidebar() {
+    this.isSidebarOpen.set(!this.isSidebarOpen());
+  }
+
+  toggleDashboardDeActive() {
+      this.dashboardActive = false;
+  }
+
+  toggleDashboardActive(){
+      this.dashboardActive=true;
   }
 
 
